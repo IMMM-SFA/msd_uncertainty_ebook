12ccf1cea1733320e4c40d4af3030b4843ffdc1f
thurber <travis.thurber@pnnl.gov>
2022-02-11 09:16:25 -0800

    landing site and deploy action

---
+++ landing/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build'
+		}),
+		paths: {
+			base: dev ? '' : '/msd_uncertainty_ebook'
+		}
+	}
+};
+
+export default config;
