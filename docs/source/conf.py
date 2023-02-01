54d2eb8c14d69a36bde3e8e8d92c43f446cd416e
crvernon <chrisrvernon@gmail.com>
2023-02-01 13:12:30 -0500

    update discovery content

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ rst_prolog = """
 .. _nb_hymod: https://uc-ebook.msdlive.org/user-redirect/lab/tree/examples/hymod.ipynb
 .. _nb_fishery_dynamics: https://uc-ebook.msdlive.org/user-redirect/lab/tree/examples/fishery_dynamics.ipynb
 .. _nb_discovery: https://uc-ebook.msdlive.org/user-redirect/lab/tree/examples/Bedford_Greene_SD_tutorial.ipynb
-.. _nb_hmm: https://uc-ebook.msdlive.org/user-redirect/lab/tree/examples/fishery_dynamics/Hidden-Markov Modeling Approaches to Creating Synthetic Streamflow Scenarios.ipynb
+.. _nb_hmm: https://uc-ebook.msdlive.org/user-redirect/lab/tree/examples/Hidden-Markov Modeling Approaches to Creating Synthetic Streamflow Scenarios.ipynb
 """
 
 
