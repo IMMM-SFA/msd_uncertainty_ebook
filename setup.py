780f3b44b2714a52674672f2b348b6f05f3c0956
erexer <13180883+erexer@users.noreply.github.com>
2024-07-09 13:38:01 -0700

    force numpy to be <2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     python_requires='>=3.6.*, <4',
     include_package_data=True,
     install_requires=[
-        "numpy>=1.21.1",
+        "numpy>=1.21.1,<2",
         "scipy>=1.8.1",
         "SALib>=1.4.4",
         "statsmodels>=0.12.2",
